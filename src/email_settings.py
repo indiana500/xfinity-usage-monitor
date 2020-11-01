9bf8c17bc0cbe4a5d9cdbe671c36da3d52c5a418
indiana500 <github@familywalden.com>
2020-11-01 17:10:30 -0500

    format update

--- src/email_settings.py
+++ src/email_settings.py
@@ -36,9 +36,8 @@ your Xfinity-Usage Monitor
 
 HIGH_LEVEL_EMAIL = """Hello,
 You current usage level is projected to be above 90% for the month.
-
 You currently have used {usage} GB.  Based on the remaining time in the month, your total data
-usage is projected to be {percentage}% of your allotment.
+usage is projected to be {percentage} % of your allotment.
 
 Regards,
 your Xfinity-Usage Monitor
